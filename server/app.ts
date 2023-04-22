9294b913e5c444acbabdf9e835faae672ba91399
Luciano Vidal <70247350+Lucho-Vidal@users.noreply.github.com>
2023-04-21 21:14:40 -0300

    first commit MEVN

---
+++ server/app.ts
@@ -0,0 +1,14 @@
+//aca se configura express
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import tasksRoutes from "./routes";
+
+const app = express();
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+
+app.use("/api", tasksRoutes)
+
+export default app;
