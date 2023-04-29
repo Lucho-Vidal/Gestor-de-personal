7b8a9a998950829661fc48bd9b7c3421bb3070cc
Luciano Vidal <70247350+Lucho-Vidal@users.noreply.github.com>
2023-04-28 21:43:21 -0300

    empece a crear el funcionamiento del itinerario

--- src/components/BuscadorTrenes.vue
+++ src/components/BuscadorTrenes.vue
@@ -14,7 +14,7 @@
                     placeholder="Buscar Tren"
                     autofocus
                     v-model="tren"
-                    v-on:change="filtroTrenes()"
+                    v-on:change="buscar()"
                 />
                 <p>Tren: {{ tren }}</p>
             </div>
@@ -57,7 +57,7 @@
                     <tr v-for="(ind, index) in indFiltrado" :key="index">
                         <td class="w-10">{{ ind.refer }}</td>
                         <td class="w-10">{{ ind.turno }}</td>
-                        <td class="w-10">{{ ind.llega }}</td>
+                        <td class="w-10">{{ ind.observaciones }}</td>
                         <td colspan="2" class="w-10">Humana Daniel</td>
                         <td class="w-10">8:40</td>
                         <td class="w-10">14:40</td>
@@ -65,6 +65,7 @@
                 </tbody>
             </table>
         </main>
+        <footer-page />
     </div>
 </template>
 
@@ -73,6 +74,9 @@ import { defineComponent } from "vue";
 import NavBar from "./NavBar.vue";
 import { getIndice } from "../services/indicesService";
 import { Indice } from "../interfaces/Indice";
+import FooterPage from "./FooterPage.vue";
+import { Itinerario } from "../interfaces/Itinerario";
+import { getItinerario } from "../services/itinerarioService";
 
 export default defineComponent({
     data() {
@@ -80,6 +84,8 @@ export default defineComponent({
             tren: "" as string,
             indice: [] as Indice[],
             indFiltrado: [] as Indice[],
+            itinerario: [] as Itinerario[],
+            itFiltrado: [] as Itinerario[],
             today: new Date(),
         };
     },
@@ -88,19 +94,39 @@ export default defineComponent({
             const res = await getIndice();
             this.indice = res.data;
         },
+        async loadItinerario() {
+            const res = await getItinerario();
+            this.itinerario = res.data;
+        },
+        buscar() {
+            this.filtroTrenes();
+            this.filtroItinerario();
+        },
+
         filtroTrenes() {
-            this.indFiltrado = this.indice.filter((ind:Indice) => {
-                    return ind.tren == parseInt(this.tren);
-                });
-            console.log('DEBUG',this.indFiltrado);
+            this.indFiltrado = this.indice.filter((ind: Indice) => {
+                return ind.tren == parseInt(this.tren);
+                //https://dev.to/duxtech/5-maneras-de-iterar-un-objeto-en-javascript-jkn
+                //https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Operators/delete
+            });
+        },
+        filtroItinerario() {
+            console.log("DEBUG2", this.itinerario);
         },
     },
     mounted() {
         this.loadIndices();
+        this.loadItinerario();
     },
     computed: {},
     components: {
         NavBar,
+        FooterPage,
     },
 });
 </script>
+<style>
+main {
+    height: 86.7vh;
+}
+</style>
