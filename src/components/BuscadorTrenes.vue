ad478334d00aa46795ea243aeb3f654644bc9b68
Luciano <luccho277@gmail.com>
2023-05-01 11:37:00 -0300

    Se implemento el filtro de turnos

--- src/components/BuscadorTrenes.vue
+++ src/components/BuscadorTrenes.vue
@@ -16,7 +16,6 @@
                     v-model="tren"
                     v-on:change="buscar()"
                 />
-                <p>Tren: {{ tren }}</p>
             </div>
             <table class="table table-striped table-hover">
                 <thead>
@@ -44,7 +43,7 @@
             </table>
             <table class="table table-striped table-hover">
                 <thead>
-                    <tr v-if="indFiltrado.length != 0 ">
+                    <tr v-if="indFiltrado.length != 0">
                         <th colspan="1">Ref</th>
                         <th colspan="1">Turno</th>
                         <th colspan="1">Observación</th>
@@ -61,6 +60,32 @@
                         <td colspan="2" class="w-10">Humana Daniel</td>
                         <td class="w-10">8:40</td>
                         <td class="w-10">14:40</td>
+                    </tr>
+                </tbody>
+            </table>
+            <table v-for="(turno, index) in turnos" :key="index" class="table table-striped table-hover">
+                <thead>
+                    <tr >
+                        <th colspan="1">ID</th>
+                        <th colspan="1">Ref</th>
+                        <th colspan="1">Tren</th>
+                        <th colspan="1">Origen</th>
+                        <th colspan="1">Sale</th>
+                        <th colspan="1">Destino</th>
+                        <th colspan="1">Llega</th>
+                        <th colspan="1">Observaciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="(vuelta, index) in turno" :key="index">
+                        <td class="w-10">{{ vuelta.vuelta }}</td>
+                        <td class="w-40">{{ vuelta.refer }}</td>
+                        <td class="w-10">{{ vuelta.tren }}</td>
+                        <td class="w-10">{{ vuelta.origen }}</td>
+                        <td class="w-10">{{ vuelta.sale }}</td>
+                        <td class="w-10">{{ vuelta.destino }}</td>
+                        <td class="w-10">{{ vuelta.llega }}</td>
+                        <td class="w-20">{{ vuelta.observaciones }}</td>
                     </tr>
                 </tbody>
             </table>
@@ -84,10 +109,11 @@ export default defineComponent({
             tren: "" as string,
             indice: [] as Indice[],
             indFiltrado: [] as Indice[],
-            turnos: [],
+            turnos: [] as Array<Indice[]>,
             itinerario: [] as Itinerario[],
             itFiltrado: [] as Itinerario[],
             today: new Date(),
+            lstTurnos: [] as Array<string[]>,
         };
     },
     methods: {
@@ -103,7 +129,6 @@ export default defineComponent({
             this.filtroTrenes();
             this.filtroItinerario();
         },
-
         filtroTrenes() {
             this.indFiltrado = this.indice.filter((ind: Indice) => {
                 return ind.tren == parseInt(this.tren);
@@ -113,6 +138,17 @@ export default defineComponent({
             this.itFiltrado = this.itinerario.filter((horarios: Itinerario) => {
                 return horarios.tren == parseInt(this.tren);
             });
+            this.filtroTurno();
+        },
+        filtroTurno() {
+            this.turnos = []
+            this.indFiltrado.forEach((turno: Indice) => {
+                this.turnos.push(
+                    this.indice.filter((ind: Indice) => {
+                        return ind.turno == turno.turno;
+                    })
+                );                
+            });            
         },
     },
     mounted() {
@@ -127,7 +163,7 @@ export default defineComponent({
 });
 </script>
 <style>
-main {
+/* main {
     height: 86.7vh;
-}
+} */
 </style>
