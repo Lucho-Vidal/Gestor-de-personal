978889171e920b18f4f6e7a0b568d759c783fb19
Luciano <luccho277@gmail.com>
2023-05-09 08:35:59 -0300

    unifique la lista de base de personal

--- src/services/personalService.ts
+++ src/services/personalService.ts
@@ -1,7 +1,5 @@
 import { IConductor } from "@/interfaces/IConductores";
-import { IGuarda } from "@/interfaces/IGuarda";
 import axios  from "./axios";
 import { AxiosResponse } from "axios";
 
 export const getConductor = async ():Promise<AxiosResponse<IConductor[]>> => await axios.get("/Personal/Ctor");
-export const getGuarda = async ():Promise<AxiosResponse<IGuarda[]>> => await axios.get("/Personal/Gda");
