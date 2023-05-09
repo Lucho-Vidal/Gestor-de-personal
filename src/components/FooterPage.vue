90bd0d1ccccfc4622f6aa03f85916afd4bcf9f90
Luciano <luccho277@gmail.com>
2023-05-09 08:33:03 -0300

    position relative

--- src/components/FooterPage.vue
+++ src/components/FooterPage.vue
@@ -13,7 +13,7 @@ footer {
     font-size: small;
     padding-top: 1rem;
     
-    position: fixed;
+    position: relative;
     bottom: 0;
     width: 100%;
     height: 60px; /* ajustar el valor según sea necesario */
