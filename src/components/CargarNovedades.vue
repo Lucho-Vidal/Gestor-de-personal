7f5e2bc83c1838ed7951e78c2735d0c47f6af59b
Luciano <luccho277@gmail.com>
2023-05-10 11:27:43 -0300

    creando el template

---
+++ src/components/CargarNovedades.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        </NavBar>
+        </footer-page>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import NavBar from "./NavBar.vue";
+
+export default defineComponent({
+    name: "App",
+    components: {
+        NavBar,
+        FooterPage
+    },
+
+})
+
+</script>
