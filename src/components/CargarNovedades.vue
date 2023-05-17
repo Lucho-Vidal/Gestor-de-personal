3869b0930048f85008dc51c467be0b2e7ebf1155
Luciano <luccho277@gmail.com>
2023-05-17 11:34:48 -0300

    se corrige importación de footer y navbar

--- src/components/CargarNovedades.vue
+++ src/components/CargarNovedades.vue
@@ -1,13 +1,14 @@
 <template>
     <div>
-        </NavBar>
-        </footer-page>
+        <NavBar/>
+        <footer-page />
     </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import NavBar from "./NavBar.vue";
+import FooterPage from './FooterPage.vue'
 
 export default defineComponent({
     name: "App",
