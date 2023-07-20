7a3f7f257f3a7da3c3c71fbdc51b5b3479a31a1f
Vidal <Luciano.Vidal@sofse.gob.ar>
2023-07-20 06:27:32 -0300

    se cambio la ruta de BaseURL

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,7 +1,8 @@
 import axios, { AxiosInstance } from "axios";
 
 const axiosInstance: AxiosInstance = axios.create({
-    baseURL: "http://localhost:3000/api",
+    //baseURL: "http://localhost:3000/api",
+    baseURL: "http://172.22.108.24:3000/api",
     headers: {
         "Content-Type": "application/json",
     },
