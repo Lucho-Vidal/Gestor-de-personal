e0213ed54baba3b8d156f1bf804223ac307f10bd
Luciano <luciano.vidal@trenesargentinos.gob.ar>
2023-08-25 11:51:58 -0300

    implementando filtro por checkbox

--- src/components/Personal.vue
+++ src/components/Personal.vue
@@ -2,13 +2,34 @@
     <main>
         <NavBar />
         <div class="container">
-            <h1 class="d-flex justify-content-center m-3">Personal</h1>
+            <h1 class="d-flex justify-content-center m-3">
+                Listado Personal de Abordo
+            </h1>
             <p class="d-flex justify-content-end m-2">
-                Fecha: {{ days[today.getDay()] }} {{ today.toLocaleDateString() }}
+                Fecha: {{ days[today.getDay()] }}
+                {{ today.toLocaleDateString() }}
             </p>
-            <table
-                class="table table-striped table-hover"
-            >
+            <div class="form-check">
+                <input
+                    class="form-check-input"
+                    type="checkbox"
+                    value=""
+                    id="flexCheckDefault"
+                />
+                <label class="form-check-label" for="flexCheckDefault">
+                    Default checkbox
+                </label>
+                <input
+                    class="form-check-input"
+                    type="checkbox"
+                    value=""
+                    id="flexCheckDefault2"
+                />
+                <label class="form-check-label" for="flexCheckDefault2">
+                    Default checkbox
+                </label>
+            </div>
+            <table class="table table-striped table-hover">
                 <thead>
                     <tr>
                         <th class="col-1" colspan="1">legajo</th>
@@ -25,7 +46,11 @@
                     </tr>
                 </thead>
                 <tbody>
-                    <tr v-for="(personal, index) in personales" :key="index" @dblclick="view()">
+                    <tr
+                        v-for="(personal, index) in personales"
+                        :key="index"
+                        @dblclick="view()"
+                    >
                         <td class="col-1">{{ personal.legajo }}</td>
                         <td class="col-1">{{ personal.apellido }}</td>
                         <td class="col-2">{{ personal.nombres }}</td>
@@ -35,9 +60,18 @@
                         <td class="col-1">{{ personal.dotacion }}</td>
                         <td class="col-2">{{ personal.observaciones }}</td>
                         <td class="col-1">{{ personal.orden }}</td>
-                        <td class="col-1"><i class="fa-solid fa-folder-open" @click="view()"></i></td>
-                        <td class="col-1"><i class="fa-solid fa-pen-to-square" @click="edit()"></i></td>
-                        
+                        <td class="col-1">
+                            <i
+                                class="fa-solid fa-folder-open"
+                                @click="view()"
+                            ></i>
+                        </td>
+                        <td class="col-1">
+                            <i
+                                class="fa-solid fa-pen-to-square"
+                                @click="edit()"
+                            ></i>
+                        </td>
                     </tr>
                 </tbody>
             </table>
@@ -50,34 +84,39 @@
 import { defineComponent } from "vue";
 import NavBar from "./NavBar.vue";
 import FooterPage from "./FooterPage.vue";
-import { getPersonales } from '../services/personalService';
+import { getPersonales } from "../services/personalService";
 import { IPersonal } from "../interfaces/IPersonal";
 
-
 export default defineComponent({
-    data(){
-        return{
-            personales:[] as IPersonal[],
+    data() {
+        return {
+            personales: [] as IPersonal[],
             today: new Date(),
-            days: ['Domingo','Lunes','Martes','Miércoles','Jueves','Viernes','Sábado']
-        }
+            days: [
+                "Domingo",
+                "Lunes",
+                "Martes",
+                "Miércoles",
+                "Jueves",
+                "Viernes",
+                "Sábado",
+            ],
+        };
     },
-    methods:{
-        async loadPersonales(){
+    methods: {
+        async loadPersonales() {
             const res = await getPersonales();
-            this.personales = res.data;            
+            this.personales = res.data;
         },
-        view(){
-            console.log("hola desde view");   
+        view() {
+            console.log("hola desde view");
         },
-        edit(){
-            console.log('hola desde Edit');
-            
-        }
+        edit() {
+            console.log("hola desde Edit");
+        },
     },
-    mounted(){
+    mounted() {
         this.loadPersonales();
-        
     },
     name: "App",
     components: {
@@ -89,4 +128,5 @@ export default defineComponent({
 <style>
 main {
     min-height: 81.6vh;
-}</style>
+}
+</style>
