1a65e8ee8ce2220686dbff44a4f48450ac91138c
Luciano <luccho277@gmail.com>
2023-09-27 11:55:03 -0300

    add createPersonal

--- src/services/personalService.ts
+++ src/services/personalService.ts
@@ -3,3 +3,8 @@ import axios  from "./axios";
 import { AxiosResponse } from "axios";
 
 export const getPersonales = async ():Promise<AxiosResponse<IPersonal[]>> => await axios.get("/Personal/Ctor");
+
+export const createPersonal= async (personal: IPersonal): Promise<AxiosResponse> => {
+    const res = await axios.post("/Personal", personal);
+    return res
+}
