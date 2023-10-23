7d008155a4b748d09d9227a7dfded95c6d4724ca
Luciano <70247350+Lucho-Vidal@users.noreply.github.com>
2023-10-23 20:39:15 -0300

    enviamos token desde axios.ts

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -3,8 +3,11 @@ import axios, { AxiosInstance } from "axios";
 const axiosInstance: AxiosInstance = axios.create({
     baseURL: "http://localhost:3000/api",
     //baseURL: "http://172.22.108.24:3000/api",
+    withCredentials:true,
     headers: {
         "Content-Type": "application/json",
+        
+        'x-access-token': localStorage.getItem('token')
     },
 });
 
