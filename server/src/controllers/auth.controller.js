3cd57057a90f5a1cca0001ff6d31c87ebd5150f5
Luciano <70247350+Lucho-Vidal@users.noreply.github.com>
2023-10-23 20:37:12 -0300

    add refreshToken

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -25,30 +25,26 @@ export const singUp = async (req, res) => {
     const savedUser = await newUser.save();
 
     const token = jwt.sign({ id: savedUser._id }, config.SECRET, {
-        expiresIn: 86400, //en segundos es un dia o 24hs
+        expiresIn: 600 //86400, //en segundos es un dia o 24hs
     });
 
     res.status(200).json({ token });
 };
+
 export const singIn = async (req, res) => {
     const userFound = await User.findOne({ email: req.body.email }).populate(
         "roles"
     );
     console.log(userFound);
     if (!userFound) return res.status(400).json({ message: "User not found" });
-
     const matchPassword = await User.comparePassword(
         req.body.password,
         userFound.password
     );
 
-    if (!matchPassword)
-        return res
-            .status(401)
-            .json({ token: null, message: "Invalid password" });
-
+    if (!matchPassword)return res.status(401).json({ token: null, message: "Invalid password" });
     const token = jwt.sign({ id: userFound._id }, config.SECRET, {
-        expiresIn: 86400,
+        expiresIn: 900 //86400,
     });
     userFound.password = "";
 
@@ -58,3 +54,24 @@ export const singIn = async (req, res) => {
         role: userFound.roles[0].name,
     });
 };
+
+export const refreshToken = async (req,res) => {
+
+    try {
+        const token = req.headers["x-access-token"];
+        const decoded = jwt.verify(token, config.SECRET);
+        const user = await User.findById(decoded.id);
+        const newToken = jwt.sign({ id: user._id }, config.SECRET, {
+            expiresIn: 900 //86400,
+        });
+        res.json({
+            token: newToken,
+            username: user.username,
+            role: user.roles[0].name,
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({error: "error de server"});
+    }
+
+}
