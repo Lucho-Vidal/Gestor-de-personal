4c5d6211cce17f1d55c14af3be3205f6d138465a
Luciano <70247350+Lucho-Vidal@users.noreply.github.com>
2023-10-23 20:40:49 -0300

    add refreshToken

--- src/services/signService.ts
+++ src/services/signService.ts
@@ -10,6 +10,11 @@ export const signIn = async (user: User): Promise<AxiosResponse> => {
     return res;
 };
 
+export const refreshToken = async (): Promise<AxiosResponse> => {
+    const res = await axios.get("/auth/refresh");
+    return res;
+};
+
 export const signUp = async (user: User): Promise<AxiosResponse> => {
     const res = await axios.post("/auth/signup", user);
     return res;
