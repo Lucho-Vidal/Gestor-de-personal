6270bf5d079e8f244ff990ed4c2b5c775467f646
Luciano <70247350+Lucho-Vidal@users.noreply.github.com>
2023-10-23 21:47:33 -0300

    withCredentials comentado

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -3,7 +3,7 @@ import axios, { AxiosInstance } from "axios";
 const axiosInstance: AxiosInstance = axios.create({
     baseURL: "http://localhost:3000/api",
     //baseURL: "http://172.22.108.24:3000/api",
-    withCredentials:true,
+    //withCredentials:true,
     headers: {
         "Content-Type": "application/json",
         
