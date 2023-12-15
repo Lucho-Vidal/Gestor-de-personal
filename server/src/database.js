bb773a42ad9e7d720071295026cf13c03d1b34fe
Luciano <luccho277@gmail.com>
2023-12-15 01:04:41 -0300

    conexión with frontend and db

--- server/src/database.js
+++ server/src/database.js
@@ -3,7 +3,8 @@ import {connect} from 'mongoose'
 
 export const startConnection = async () => {
         try {
-            const db = await connect("mongodb://127.0.0.1/trenesArgentinos");
+            //const db = await connect("mongodb://127.0.0.1/trenesArgentinos");
+            const db = await connect("mongodb+srv://luccho277:hh2ibnBn!3NYCeG@gnpa.b3jyho6.mongodb.net/?retryWrites=true&w=majority")
             console.log('DB is connected:',db.connection.name);
         } catch (error) {
             console.log(error+"------DEBUG");
