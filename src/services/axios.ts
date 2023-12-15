5f889a0706ddb96e73754657de28ecac889be77c
Luciano <luccho277@gmail.com>
2023-12-15 04:27:26 -0300

    baseURL render

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,8 +1,9 @@
 import axios, { AxiosInstance } from "axios";
 
 const axiosInstance: AxiosInstance = axios.create({
-    baseURL: "http://localhost:3000/api",
+    //baseURL: "http://localhost:3000/api",
     //baseURL: "http://172.22.108.24:3000/api",
+    baseURL: "https://gnpd.onrender.com",
     //withCredentials:true,
     headers: {
         "Content-Type": "application/json",
