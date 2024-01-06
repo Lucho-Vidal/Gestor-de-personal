3e2bfde9fd286057f709f46c8142e26375965d9f
Luciano <70247350+Lucho-Vidal@users.noreply.github.com>
2024-01-06 20:44:01 -0300

    db env

--- server/src/database.js
+++ server/src/database.js
@@ -5,7 +5,8 @@ env.config();
 export const startConnection = async () => {
     try {
         //const db = await connect("mongodb://127.0.0.1/trenesArgentinos");
-        const db = await connect(process.env.DATABASE_URL);
+        const dbURL = process.env.DATABASE_URL
+        const db = await connect(dbURL);
         console.log("DB is connected:", db.connection.name);
     } catch (error) {
         console.log(error + "------DEBUG");
