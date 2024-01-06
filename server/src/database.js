acc6811a80c534dce70d910b86c7a936fbefe843
Luciano <70247350+Lucho-Vidal@users.noreply.github.com>
2024-01-06 20:37:47 -0300

    implemento .env

--- server/src/database.js
+++ server/src/database.js
@@ -1,12 +1,13 @@
+import { connect } from "mongoose";
+import env from "dotenv";
 
-import {connect} from 'mongoose'
-
+env.config();
 export const startConnection = async () => {
-        try {
-            //const db = await connect("mongodb://127.0.0.1/trenesArgentinos");
-            const db = await connect("mongodb+srv://luccho277:hh2ibnBn!3NYCeG@gnpa.b3jyho6.mongodb.net/?retryWrites=true&w=majority")
-            console.log('DB is connected:',db.connection.name);
-        } catch (error) {
-            console.log(error+"------DEBUG");
-        }
-    };
+    try {
+        //const db = await connect("mongodb://127.0.0.1/trenesArgentinos");
+        const db = await connect(process.env.DATABASE_URL);
+        console.log("DB is connected:", db.connection.name);
+    } catch (error) {
+        console.log(error + "------DEBUG");
+    }
+};
