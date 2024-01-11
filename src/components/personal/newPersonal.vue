6f7c72116436ca7f6fef52a9c4914cb0504f7796
Luciano <70247350+Lucho-Vidal@users.noreply.github.com>
2024-01-11 01:16:48 -0300

    cambien input de conocimiento por checkbox

--- src/components/personal/newPersonal.vue
+++ src/components/personal/newPersonal.vue
@@ -7,11 +7,10 @@
             </h1>
             <div class="alert alert-danger" role="alert" v-if="alerta">
                 <h4 class="alert-heading">ATENCIÓN!</h4>
-                <hr>
+                <hr />
                 {{ alerta }}
             </div>
             <!-- <modalBuscarPersonal :personales="personales" /> -->
-
 
             <form @submit.prevent="savePersonal()" class="row">
                 <div class="row">
@@ -55,7 +54,6 @@
                     </div>
                 </div>
                 <div class="row">
-                    
                     <div class="col-1">
                         <label for="base">Base </label>
                         <select
@@ -63,7 +61,6 @@
                             id="base"
                             class="form-control mb-3"
                             v-model="newPersonal.dotacion"
-                            
                         >
                             <option value="PC">PC</option>
                             <option value="LLV">LLV</option>
@@ -74,7 +71,7 @@
                             <option value="AK">AK</option>
                         </select>
                     </div>
-                    
+
                     <div class="col-3">
                         <label for="especialidad">Especialidad </label>
                         <select
@@ -84,12 +81,24 @@
                             v-model="newPersonal.especialidad"
                             required
                         >
-                            <option value="Conductor electrico">Conductor Eléctrico</option>
-                            <option value="Conductor diesel">Conductor Diesel</option>
-                            <option value="Ayudante habilitado">Ayudante Habilitado</option>
-                            <option value="Ayudante conductor">Ayudante Conductor</option>
-                            <option value="GuardaTren electrico">GuardaTren Eléctrico</option>
-                            <option value="GuardaTren diesel">GuardaTren Diesel</option>
+                            <option value="Conductor electrico">
+                                Conductor Eléctrico
+                            </option>
+                            <option value="Conductor diesel">
+                                Conductor Diesel
+                            </option>
+                            <option value="Ayudante habilitado">
+                                Ayudante Habilitado
+                            </option>
+                            <option value="Ayudante conductor">
+                                Ayudante Conductor
+                            </option>
+                            <option value="GuardaTren electrico">
+                                GuardaTren Eléctrico
+                            </option>
+                            <option value="GuardaTren diesel">
+                                GuardaTren Diesel
+                            </option>
                         </select>
                     </div>
                     <div class="col-2">
@@ -112,13 +121,13 @@
                             v-model="newPersonal.franco"
                             required
                         >
-                            <option value=0>Domingo</option>
-                            <option value=1>Lunes</option>
-                            <option value=2>Martes</option>
-                            <option value=3>Miércoles</option>
-                            <option value=4>Jueves</option>
-                            <option value=5>Viernes</option>
-                            <option value=6>Sábado</option>
+                            <option value="0">Domingo</option>
+                            <option value="1">Lunes</option>
+                            <option value="2">Martes</option>
+                            <option value="3">Miércoles</option>
+                            <option value="4">Jueves</option>
+                            <option value="5">Viernes</option>
+                            <option value="6">Sábado</option>
                         </select>
                     </div>
                     <div class="col-2">
@@ -134,123 +143,80 @@
                     </div>
                 </div>
 
-                <div class="row" v-if="newPersonal.especialidad.includes('Conductor') || newPersonal.especialidad.includes('Ayudante')" >
+                <div
+                    class="row"
+                    v-if="
+                        newPersonal.especialidad.includes('Conductor') ||
+                        newPersonal.especialidad.includes('Ayudante')
+                    "
+                >
                     <h5>Conocimientos</h5>
                     <div class="col-1">
                         <label for="turno"></label>
                         CML
-                        <select
-                            name="CML"
-                            id="CML"
-                            class="form-control mb-3"
+                        <input
+                            type="checkbox"
                             v-model="newPersonal.conocimientos.CML"
-                            required
-                        >
-                            <option value="NO">NO</option>
-                            <option value="SI">SI</option>
-                        </select>
+                        />
                     </div>
                     <div class="col-1">
                         <label for="turno"></label>
                         CKD
-                        <select
-                            name="CKD"
-                            id="CKD"
-                            class="form-control mb-3"
+                        <input
+                            type="checkbox"
                             v-model="newPersonal.conocimientos.CKD"
-                            required
-                        >
-                            <option value="NO">NO</option>
-                            <option value="SI">SI</option>
-                        </select>
+                        />
                     </div>
                     <div class="col-1">
                         <label for="turno"></label>
                         RO
-                        <select
-                            name="RO"
-                            id="RO"
-                            class="form-control mb-3"
+                        <input
+                            type="checkbox"
                             v-model="newPersonal.conocimientos.RO"
-                            required
-                        >
-                            <option value="NO">NO</option>
-                            <option value="SI">SI</option>
-                        </select>
+                        />
                     </div>
                     <div class="col-1">
                         <label for="turno"></label>
                         MPN
-                        <select
-                            name="MPN"
-                            id="MPN"
-                            class="form-control mb-3"
+                        <input
+                            type="checkbox"
                             v-model="newPersonal.conocimientos.MPN"
-                            required
-                        >
-                            <option value="NO">NO</option>
-                            <option value="SI">SI</option>
-                        </select>
+                        />
                     </div>
                     <div class="col-1">
                         <label for="turno"></label>
                         OL
-                        <select
-                            name="OL"
-                            id="OL"
-                            class="form-control mb-3"
+                        <input
+                            type="checkbox"
                             v-model="newPersonal.conocimientos.OL"
-                            required
-                        >
-                            <option value="NO">NO</option>
-                            <option value="SI">SI</option>
-                        </select>
+                        />
                     </div>
                     <div class="col-1">
                         <label for="turno"></label>
                         LCI
-                        <select
-                            name="LCI"
-                            id="LCI"
-                            class="form-control mb-3"
+                        <input
+                            type="checkbox"
                             v-model="newPersonal.conocimientos.LCI"
-                            required
-                        >
-                            <option value="NO">NO</option>
-                            <option value="SI">SI</option>
-                        </select>
+                        />
                     </div>
                     <div class="col-1">
                         <label for="turno"></label>
                         ELEC
-                        <select
-                            name="ELEC"
-                            id="ELEC"
-                            class="form-control mb-3"
+                        <input
+                            type="checkbox"
                             v-model="newPersonal.conocimientos.ELEC"
-                            required
-                        >
-                            <option value="NO">NO</option>
-                            <option value="SI">SI</option>
-                        </select>
+                        />
                     </div>
                     <div class="col-1">
                         <label for="turno"></label>
                         DUAL
-                        <select
-                            name="DUAL"
-                            id="DUAL"
-                            class="form-control mb-3"
+                        <input
+                            type="checkbox"
                             v-model="newPersonal.conocimientos.DUAL"
-                            required
-                        >
-                            <option value="NO">NO</option>
-                            <option value="SI">SI</option>
-                        </select>
+                        />
                     </div>
-                    
                 </div>
-                
+
                 <div class="row">
                     <div class="col">
                         <label for="detalle"></label>
@@ -262,12 +228,12 @@
                         ></textarea>
                     </div>
                 </div>
-                                
 
                 <button class="btn btn-primary col-1 m-2">Guardar</button>
-                <i class="btn btn-secondary col-1 m-2" @click="cerrar()">Cerrar</i>
+                <i class="btn btn-secondary col-1 m-2" @click="cerrar()"
+                    >Cerrar</i
+                >
             </form>
-            
         </main>
 
         <footer-page />
@@ -279,11 +245,9 @@ import { defineComponent } from "vue";
 import NavBar from "../NavBar.vue";
 import FooterPage from "../FooterPage.vue";
 
-import { getPersonales , createPersonal } from "../../services/personalService";
+import { getPersonales, createPersonal } from "../../services/personalService";
 import { IPersonal } from "../../interfaces/IPersonal";
 import { newToken } from "../../services/signService";
-
-
 
 export default defineComponent({
     data() {
@@ -298,15 +262,15 @@ export default defineComponent({
                 dotacion: "",
                 observaciones: "",
                 orden: 0,
-                conocimientos:  {
-                CML: "NO",
-                CKD: "NO",
-                RO: "NO",
-                MPN: "NO",
-                OL: "NO",
-                LCI: "NO",
-                ELEC: "NO",
-            }
+                conocimientos: {
+                    CML: false,
+                    CKD: false,
+                    RO: false,
+                    MPN: false,
+                    OL: false,
+                    LCI: false,
+                    ELEC: false,
+                },
             } as IPersonal,
             personales: [] as IPersonal[],
             days: [
@@ -324,8 +288,10 @@ export default defineComponent({
     methods: {
         async savePersonal() {
             try {
-                if(this.alerta){
-                    this.alerta =  " ATENCIÓN!!! NO ES POSIBLE CARGAR ESTA NOVEDAD!      " + this.alerta 
+                if (this.alerta) {
+                    this.alerta =
+                        " ATENCIÓN!!! NO ES POSIBLE CARGAR ESTA NOVEDAD!      " +
+                        this.alerta;
                     return;
                 }
                 console.log(this.newPersonal);
@@ -335,7 +301,7 @@ export default defineComponent({
                 console.error(error);
             }
         },
-        cerrar(){
+        cerrar() {
             this.$router.push({ name: "Personal" });
         },
         /* Este método trae la lista de todos los personales */
@@ -345,13 +311,12 @@ export default defineComponent({
         },
         searchPersonalPorLegajo() {
             this.alerta = "";
-            this.personales.forEach(
-                (personal: IPersonal) => {
-                    if (personal.legajo == this.newPersonal.legajo){
-                        this.alerta = 'El personal ya existe!!! No es posible carga dos personales con el mismo legajo'
-                    }
+            this.personales.forEach((personal: IPersonal) => {
+                if (personal.legajo == this.newPersonal.legajo) {
+                    this.alerta =
+                        "El personal ya existe!!! No es posible carga dos personales con el mismo legajo";
                 }
-            );
+            });
         },
     },
     mounted() {
