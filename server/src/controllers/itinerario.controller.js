c73834e49091caadb75fbbd91ee4d363712fba2b
Luciano <70247350+Lucho-Vidal@users.noreply.github.com>
2024-01-23 21:05:53 -0300

    avanzando con el itinerario

--- server/src/controllers/itinerario.controller.js
+++ server/src/controllers/itinerario.controller.js
@@ -4,3 +4,19 @@ export const getItinerario = async (req, res) => {
     const itinerario = await Itinerario.find();
     res.json(itinerario);
 };
+
+export const getItinerarioPaginado = async (req, res) => {
+    const { page, pageSize } = req.query;
+
+    try {
+        const result = await Itinerario.find()
+            .skip((page - 1) * pageSize)
+            .limit(pageSize);
+
+        res.status(200).json(result);
+    } catch (error) {
+        console.error(error);
+        // Manejar el error y enviar una respuesta de error al cliente
+        res.status(500).json({ error: 'Error al obtener datos del itinerario.' });
+    }
+}
