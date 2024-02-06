77462af6ecf17c198189fd4c0b7de17b3081c894
Luciano <luccho277@gmail.com>
2024-02-06 08:31:14 -0300

    cambio a entorno dev

--- server/src/database.js
+++ server/src/database.js
@@ -4,9 +4,9 @@ import env from "dotenv";
 env.config();
 export const startConnection = async () => {
     try {
-        //const db = await connect("mongodb://127.0.0.1/trenesArgentinos");
-        const dbURL = process.env.DATABASE_URL
-        const db = await connect(dbURL);
+        const db = await connect("mongodb://127.0.0.1/trenesArgentinos");
+        // const dbURL = process.env.DATABASE_URL
+        // const db = await connect(dbURL);
         console.log("DB is connected:", db.connection.name);
     } catch (error) {
         console.log(error + "------DEBUG");
--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,9 +1,9 @@
 import axios, { AxiosInstance } from "axios";
 
 const axiosInstance: AxiosInstance = axios.create({
-    //baseURL: "http://localhost:3000/api",
+    baseURL: "http://localhost:3000/api",
     //baseURL: "http://172.22.108.24:3000/api",
-    baseURL: "https://gnpd.onrender.com/api",
+    //baseURL: "https://gnpd.onrender.com/api",
     //withCredentials:true,
     headers: {
         "Content-Type": "application/json",
