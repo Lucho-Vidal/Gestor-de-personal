1f17cdb482bd87226acfd307efafd7086983e021
Luciano <luccho277@gmail.com>
2024-03-19 11:56:21 -0300

    se corrige importacion de bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { createApp } from "vue";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './styles.css'
 import App from "./App.vue";
 import router from "./router";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 
 
