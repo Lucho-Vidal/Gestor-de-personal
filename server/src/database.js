fdd740927746ba8855374c3651c394d3a697403a
Luciano <luccho277@gmail.com>
2024-03-30 12:03:02 -0300

    bug cors

--- server/src/database.js
+++ server/src/database.js
@@ -4,7 +4,7 @@ import env from "dotenv";
 env.config();
 export const startConnection = async () => {
     try {
-        const db = await connect("mongodb://127.0.0.1/trenesArgentinos");
+        const db = await connect("mongodb://127.0.0.1/trenesArgentinos") || await connect("mongodb://mongo/trenesArgentinos") ;
         //const db = await connect("mongodb://mongo/trenesArgentinos");
         // const dbURL = process.env.DATABASE_URL
         // const db = await connect(dbURL);
