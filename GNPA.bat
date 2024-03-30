c6acc9c2ef5d6414f9c4549808511799200968b7
Alegre <fernando.alegre@sofse.gob.ar>
2024-03-30 08:56:29 -0300

    Se actualizo la lanzadera

---
+++ GNPA.bat
@@ -0,0 +1,21 @@
+@echo off
+rem Inicia el servidor frontend
+echo Iniciando el servidor frontend 5173...
+cd E:\GNPA\GNPA2\Gestor-de-personal\
+start pnpm run dev --host
+
+rem Espera un momento para que el servidor frontend se inicie completamente
+timeout /t 5
+
+rem Inicia el servidor backend
+echo Iniciando el servidor backend...
+cd E:\GNPA\GNPA2\Gestor-de-personal\server
+start pnpm run dev
+
+rem Espera un momento para que el servidor frontend se inicie completamente
+timeout /t 5
+
+rem Inicia el servidor frontend
+echo Iniciando el servidor frontend 8080...
+cd E:\GNPA\Gestor-de-personal
+start npm run serve 
